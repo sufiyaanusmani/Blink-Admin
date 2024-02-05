3182ec186d69522b2eea7d4b6d02556e0700db56
Sufiyaan Usmani <usmanisufiyaan@gmail.com>
2024-02-05 21:15:17 +0500

    integrate django with firebase

--- blinkadmin/base/views.py
+++ blinkadmin/base/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render, HttpResponse
+import pyrebase
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+cred = credentials.Certificate("blink-a34ae-firebase-adminsdk-5myau-fd79745951.json")
+firebase_admin.initialize_app(cred, {"databaseURL": "https://blink-a34ae.firebaseio.com"})
+db = firestore.client()
 
 # Create your views here.
 
 
 def homePage(request):
+    ref = db.collection("customers")
+    docs = ref.stream()
+
+    for doc in docs:
+        print(f"{doc.id} => {doc.to_dict()}")
     context = {}
     return render(request, 'base/index.html')
 
