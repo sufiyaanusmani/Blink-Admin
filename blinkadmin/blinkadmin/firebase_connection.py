1090fbdd6031efeeace019e1ab5aa3781dc048b2
Sufiyaan Usmani <usmanisufiyaan@gmail.com>
2024-03-03 01:08:58 +0500

    deployment settings 3

--- blinkadmin/blinkadmin/firebase_connection.py
+++ blinkadmin/blinkadmin/firebase_connection.py
@@ -3,7 +3,7 @@ from firebase_admin import credentials
 
 def initialize_firebase():
     try:
-        cred = credentials.Certificate("../blink-a34ae-firebase-adminsdk-5myau-fd79745951.json")
+        cred = credentials.Certificate("blink-a34ae-firebase-adminsdk-5myau-fd79745951.json")
         firebase_admin.initialize_app(cred, {"databaseURL": "https://blink-a34ae.firebaseio.com"})
     except Exception as e:
         print(e)
